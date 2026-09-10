a6e877e6755523927f381293d83ebc05072c9e0e
Nikhil Reddy <nikhilreddy6179@gmail.com>
2026-09-10 15:55:50 -0400

    fix writing routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel";
 
 export default defineConfig({
-  site: "https://www.nikhilreddy.com",
+  site: "https://www.nikreddy.com",
   output: "server",
   adapter: vercel(),
   publicDir: "./assets"
